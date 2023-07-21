b086950f3f9fb33449ce69374abf61dfd7356734
IWhitebird <115157819+IWhitebird@users.noreply.github.com>
2023-07-21 22:20:20 +0530

    done i am done

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,13 +6,12 @@ const Home = ({ lat, lng }) => {
   const mapContainerRef = useRef(null);
   const streetViewContainerRef = useRef(null);
   const mapRef = useRef(null);
-  const [markers, setMarkers] = useState([]);
-
+  const [markers, setMarkers] = useState();
+  
   useEffect(() => {
     const loadGoogleMapScript = () => {
       const script = document.createElement('script');
       script.src = `https://maps.googleapis.com/maps/api/js?key=${env.GOOGLE_API_KEY}&callback=initMap`;
-      console.log(script.src)
       script.async = true;
       script.defer = true;
       window.initMap = initMap;
@@ -21,53 +20,54 @@ const Home = ({ lat, lng }) => {
 
     const initMap = () => {
 
-      const mapOptions = {
-        center: { lat, lng },
-        zoom: 15,
-        disableDefaultUI: true, // Hide the default user interface controls
-        mapTypeControl: false, // Remove the map type controls
-        mapTypeId: window.google.maps.MapTypeId.ROADMAP, // Use the default map type without additional information
-        styles: [
-          {
-            featureType: 'all',
-            elementType: 'labels',
-            stylers: [
-              { visibility: 'off' },
-            ],
-          },
-          {
-            featureType: 'landscape',
-            elementType: 'labels',
-            stylers: [
-              { visibility: 'on' },
-            ],
-          },
-          {
-            featureType: 'administrative.country',
-            elementType: 'labels',
-            stylers: [
-              { visibility: 'on' },
-            ],
-          },
-          {
-            featureType: 'administrative.locality',
-            elementType: 'labels',
-            stylers: [
-              { visibility: 'on' },
-            ],
-          },
-        ],
-      };
-      
 
-    const panoramaOptions = {
-      position: { lat, lng },
-      pov: { heading: 0, pitch: 0 },
-      zoom: 1,
-      panoProviderOptions: {
-        panoId: 'gs_id:remove_labels',
-      },
-    };
+      const mapOptions = {
+         center: { lat, lng },
+          zoom: 15,
+          disableDefaultUI: true, // Hide the default user interface controls
+          mapTypeControl: false, // Remove the map type controls
+          mapTypeId: window.google.maps.MapTypeId.ROADMAP, // Use the default map type without additional information
+          styles: [
+            {
+              featureType: 'all',
+              elementType: 'labels',
+              stylers: [
+                { visibility: 'off' },
+              ],
+            },
+            {
+              featureType: 'landscape',
+              elementType: 'labels',
+              stylers: [
+                { visibility: 'on' },
+              ],
+            },
+            {
+              featureType: 'administrative.country',
+              elementType: 'labels',
+              stylers: [
+                { visibility: 'on' },
+              ],
+            },
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels',
+              stylers: [
+                { visibility: 'on' },
+              ],
+            },
+          ],
+        };
+        
+      
+       const panoramaOptions = {
+         position: { lat, lng },
+          pov: { heading: 0, pitch: 0 },
+          zoom: 1,
+          panoProviderOptions: {
+            panoId: 'gs_id:remove_labels',
+          },
+        };
 
       const map = new window.google.maps.Map(mapContainerRef.current, mapOptions);
       mapRef.current = map;
@@ -82,8 +82,8 @@ const Home = ({ lat, lng }) => {
       map.setStreetView(panorama);
 
       // Add click event listener to the map
-      map.addListener('click', (event) => {
-        removeMarkers(); // Clear existing markers
+      map.addListener ('click', (event) => {
+         // Clear existing markers
         placeMarker(event.latLng);
       });
     };
@@ -97,23 +97,22 @@ const Home = ({ lat, lng }) => {
 
   // Function to place a marker on the map
   const placeMarker = (location) => {
+
+    removeMarkers();
+
     const marker = new window.google.maps.Marker({
       position: location,
       map: mapRef.current,
     });
-
-    setMarkers((prevMarkers) => [...prevMarkers, marker]);
-
-    // Center the map on the marker's location
-    mapRef.current.setCenter(location);
+    setMarkers(marker);
   };
 
-  // Function to remove all existing markers from the map
   const removeMarkers = () => {
-    markers.forEach((marker) => {
-      marker.setMap(null);
-    });
-    setMarkers([]);
+    if (typeof markers != undefined) {
+      console.log(markers)
+      markers.setMap(null);
+      setMarkers(null);
+    }
   };
 
   return (
