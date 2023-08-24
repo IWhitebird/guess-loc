525fbd17976633797264c49f969d5d2628878175
IWhitebird <115157819+IWhitebird@users.noreply.github.com>
2023-08-24 19:48:51 +0530

    uypodatetoo

--- server/db.js
+++ server/db.js
@@ -1,26 +1,46 @@
-// const Pool = require('pg').Pool
-// const dotenv = require('dotenv');
+const Pool = require('pg').Pool
+const dotenv = require('dotenv');
+require('dotenv').config();
+
+
+//LOCAL
 
 // const pool = new Pool({
-//     user: "postgres",
-//     password: 'admin',
-//     host: 'localhost',
-//     post: '5432',
-//     database: 'geobase',
-// });
-
-// module.exports = pool;
-
-
-const Pool = require('pg').Pool;
-const dotenv = require('dotenv');
-
-dotenv.config();
+  //     user: "postgres",
+  //     password: 'admin',
+  //     host: 'localhost',
+  //     post: '5432',
+  //     database: 'geobase',
+  // });
+  
+  // module.exports = pool;
 
 
-// Create a new pool instance
-const pool = new Pool({
-  connectionString: process.env.DB_STRING + "?sslmode=require"
-});
+  //RENDER
+  
+  // const pool = new Pool({
+  //   connectionString: process.env.DB_STRING + "?sslmode=require"
+  // });
+  
+  // module.exports = pool;
 
-module.exports = pool;
+  const pool = new Pool({
+    connectionString: process.env.NEON_DB_STRING ,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  })
+  
+  async function getPostgresVersion() {
+    const client = await pool.connect();
+    try {
+      const res = await client.query('SELECT version()');
+      console.log(res.rows[0]);
+    } finally {
+      client.release();
+    }
+  }
+  
+  getPostgresVersion();
+
+  module.exports = pool;
